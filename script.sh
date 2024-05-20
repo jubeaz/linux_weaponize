d859c4cdbbf626075ed71180def69f2277274591
jubeaz <pascal.auillans@gmail.com>
2024-05-20 13:44:31 +0200

    update

--- script.sh
+++ script.sh
@@ -19,29 +19,24 @@ mkdir -p sh
 
 
 echo "chisel"
-#__get_github_release jpillora/chisel 'linux_(386|amd64)'
 eget jpillora/chisel -s linux/amd64 --download-only --upgrade-only --to ./bin
 eget jpillora/chisel -s linux/386 --download-only  --upgrade-only --to ./bin
 
 
 echo "nmap"
-#__get_github_release opsec-infosec/nmap-static-binaries 'nmap-x86_64.tar.gz' 
 eget opsec-infosec/nmap-static-binaries -a nmap-x86_64 --download-only  --to ./bin
 
 
 echo "pspy"
 __get_github_release DominicBreuker/pspy "pspy" 
 mv pspy* bin
-#eget  DominicBreuker/pspy  --download-only --upgrade-only --to ./bin
 
 echo "lineas"
-#__get_github_release peass-ng/PEASS-ng "(linpeas_linux_amd64|linpeas_linux_386|linpeas.sh)"
 eget  peass-ng/PEASS-ng -a linpeas.sh --download-only --upgrade-only --to ./sh
 eget  peass-ng/PEASS-ng -s linux/amd64 --download-only  --upgrade-only --to ./bin
 eget  peass-ng/PEASS-ng -s linux/386 --download-only --upgrade-only --to ./bin
 
 echo 'Pretender'
-#__get_github_release RedTeamPentesting/pretender "Windows"
 eget RedTeamPentesting/pretender -a Linux_x86_64 --download-only --upgrade-only --to ./bin
 
 
